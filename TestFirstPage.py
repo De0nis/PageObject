55ba8f93114875ac2db8a930ad6fcc14a9716067
Denis <96110531+De0nis@users.noreply.github.com>
2022-01-28 13:12:15 +0700

    Add files via upload

---
+++ TestFirstPage.py
@@ -0,0 +1,25 @@
+from Firstpage import Titlepage
+import pytest
+@pytest.fixture(scope='session')
+def url():
+    url='https://yandex.ru'
+    return url
+
+class TestTitlepage:
+    @pytest.mark.smoke 
+    def test_find_search_field(self,browser):
+        Titlepage.should_be_located_search_field(self,browser)
+ 
+    def test_key_send_search_field(self,browser):
+        Titlepage.shold_be_key_send_field_text(self,browser)
+    @pytest.mark.smoke 
+    def test_find_mail_link(self,browser):
+        Titlepage.should_be_located_search_field(self,browser)
+    @pytest.mark.smoke 
+    def test_login_form_on_titlepage(self,browser):
+        Titlepage.should_be_located_loginform(self,browser)
+    @pytest.mark.smoke 
+    def  test_login_form_open(self,browser):
+         Titlepage.should_be_open_loginform(self,browser)
+
+      
