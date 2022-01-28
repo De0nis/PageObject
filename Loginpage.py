f18b36f720eec45979d386473a9451e65156d675
Denis <96110531+De0nis@users.noreply.github.com>
2022-01-28 13:10:46 +0700

    Add files via upload

---
+++ Loginpage.py
@@ -0,0 +1,16 @@
+from BasePage import Base
+from Locators import LoginpageLocators
+
+class Loginpage(Base):
+
+    def should_be_login_form(self,browser):  
+        assert Base.is_element_present(self,browser,(LoginpageLocators.LOGIN_ID_FIELD)), (
+              "Login form is not presented")
+
+    def should_send_key_to_form(self,browser,key):
+        browser.get('https://passport.yandex.ru')     
+        Base.is_key_sending(self,browser,key,(LoginpageLocators.LOGIN_ID_FIELD))
+        Base.press_button(self,browser,(LoginpageLocators.LOGIN_CONFORM_BUTTON)) 
+        Base.is_element_present(self,browser,(LoginpageLocators.LOGIN_INCORRECT))
+
+   
