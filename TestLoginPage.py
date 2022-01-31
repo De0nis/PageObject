0ae236931bca55360405e892ddc753b02eed1e05
Denis <96110531+De0nis@users.noreply.github.com>
2022-01-31 22:39:02 +0700

    Update TestLoginPage.py
    test_sending_sql_injection added

--- TestLoginPage.py
+++ TestLoginPage.py
@@ -27,4 +27,6 @@ class Testloginepage:
     def test_sending_correct_keys_phone(self,browser,key='89999999999'):    
         Loginpage.should_send_key_to_form(self,browser,key)
     
+    def test_sending_sql_injection(self,browser,key='$id = $_GETid;$query = "SELECT * FROM news WHERE id=$id'):    
+        Loginpage.should_send_key_to_form(self,browser,key)
     
