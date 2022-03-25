d601a6929106ba5d5fd3d222fe57cd8667b8ebb8
Denis <96110531+De0nis@users.noreply.github.com>
2022-03-25 10:59:27 +0700

    Update Locators.py

--- Locators.py
+++ Locators.py
@@ -1,17 +1,17 @@
 from selenium.webdriver.common.by import By
 """locators_by_pages"""
 class LoginpageLocators(object):
-    LOGIN_ID_FIELD = (By.ID,"passp-field-login")
-    LOGIN_INCORRECT=(By.ID,"passp-field-passwd")
-    LOGIN_INCORRECT2=(By.ID,"passp-field-confirmation-code")
+    LOGIN_ID_FIELD = (By.ID, "passp-field-login")
+    LOGIN_INCORRECT = (By.ID, "passp-field-passwd")
+    LOGIN_INCORRECT2 = (By.ID, "passp-field-confirmation-code")
     
-    LOGIN_INCORRECT_ANSWER=('text')
-    LOGIN_CONFORM_BUTTON=(By.ID,"passp:sign-in")
-    LOGIN_PASS = (By.ID,"passp-field-phoneCode")
-    LOGIN_PASS2 = (By.ID,"passp-field-confirmation-code")
+    LOGIN_INCORRECT_ANSWER = ('text')
+    LOGIN_CONFORM_BUTTON = (By.ID, "passp:sign-in")
+    LOGIN_PASS = (By.ID, "passp-field-phoneCode")
+    LOGIN_PASS2 = (By.ID, "passp-field-confirmation-code")
 class FirstpagePageLocators(object):
-    LOGIN_FORM=(By.LINK_TEXT,"Войти")
-    LOGIN_FORM2=(By.CLASS_NAME,"passp-login-form")
-    MAIL_LOCATOR=(By.LINK_TEXT,"Почта")
-    SEARCH_FIELD=(By.ID, "text")
-    SEARCH_KEY=('ян')
+    LOGIN_FORM = (By.LINK_TEXT, "Войти")
+    LOGIN_FORM2 = By.CLASS_NAME, "passp-login-form")
+    MAIL_LOCATOR = (By.LINK_TEXT, "Почта")
+    SEARCH_FIELD = (By.ID, "text")
+    SEARCH_KEY = ('ян')
