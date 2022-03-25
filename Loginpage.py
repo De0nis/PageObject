89a69f5383b7ba9da1d52d0ec94495fcb0acf1b8
Denis <96110531+De0nis@users.noreply.github.com>
2022-03-25 11:02:20 +0700

    Update Loginpage.py

--- Loginpage.py
+++ Loginpage.py
@@ -2,16 +2,13 @@ from BasePage import Base
 from Locators import LoginpageLocators
 
 class Loginpage(Base):
-
-    def should_be_login_form(self,browser):  
-        assert Base.is_element_present(self,browser,(LoginpageLocators.LOGIN_ID_FIELD)), (
-              "Login form is not presented")
-
-    def should_send_key_to_form(self,browser,key):
+    def should_be_login_form(self, browser):  
+        assert Base.is_element_present(self, browser, (LoginpageLocators.LOGIN_ID_FIELD)), ("Login form is not presented")
+    def should_send_key_to_form(self, browser, key):
         browser.get('https://passport.yandex.ru')     
-        Base.is_key_sending(self,browser,key,(LoginpageLocators.LOGIN_ID_FIELD))
-        Base.press_button(self,browser,(LoginpageLocators.LOGIN_CONFORM_BUTTON)) 
-        assert (Base.is_element_present(self,browser,(LoginpageLocators.LOGIN_INCORRECT)))or(Base.is_element_present(self,browser,(LoginpageLocators.LOGIN_INCORRECT2))),(
-        "Login form is not presented")
+        Base.is_key_sending(self, browser, key, (LoginpageLocators.LOGIN_ID_FIELD))
+        Base.press_button(self, browser, (LoginpageLocators.LOGIN_CONFORM_BUTTON)) 
+        assert (Base.is_element_present(self, browser, (LoginpageLocators.LOGIN_INCORRECT))) or (Base.is_element_present(self, browser, 
+        (LoginpageLocators.LOGIN_INCORRECT2))), ("Login form is not presented")
 
    
